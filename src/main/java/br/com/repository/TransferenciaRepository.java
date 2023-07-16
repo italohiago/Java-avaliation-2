73888431c231b10366b007952790e7495ca108ea
Italo Hiago <italohiagolopes@outlook.com>
2023-07-16 15:16:07 -0300

    Correcao TransferenciaRepository

--- src/main/java/br/com/repository/TransferenciaRepository.java
+++ src/main/java/br/com/repository/TransferenciaRepository.java
@@ -2,24 +2,34 @@ package br.com.repository;
 
 import br.com.model.Transferencia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
 
-    List<Transferencia> findByContaId(Long contaId);
+    // Método para buscar transferências por número da conta
+    List<Transferencia> findByContaId(Long numeroConta);
 
+    // Método para buscar transferências por período de tempo
     List<Transferencia> findByDataTransferenciaBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
 
-    List<Transferencia> findByNomeOperadorTransacao(String nomeOperadorTransacao);
+    // Método para buscar transferências por nome do operador
+    List<Transferencia> findByNomeOperadorTransacao(String nomeOperador);
 
+    // Método para buscar transferências com filtros (número da conta, período e nome do operador)
+    @Query("SELECT t FROM Transferencia t " +
+            "WHERE (:numeroConta IS NULL OR t.conta.id = :numeroConta) " +
+            "AND (:dataInicial IS NULL OR t.dataTransferencia >= :dataInicial) " +
+            "AND (:dataFinal IS NULL OR t.dataTransferencia <= :dataFinal) " +
+            "AND (:nomeOperador IS NULL OR t.nomeOperadorTransacao = :nomeOperador)")
     List<Transferencia> findByContaIdAndDataTransferenciaBetweenAndNomeOperadorTransacao(
-            Long contaId, LocalDateTime dataInicial, LocalDateTime dataFinal, String nomeOperadorTransacao
+            Long numeroConta, LocalDateTime dataInicial, LocalDateTime dataFinal, String nomeOperador
     );
 
-    // Método para buscar transferências de uma conta em um período de tempo
-    @Query("SELECT t FROM Transferencia t WHERE t.conta.id = :numeroConta AND t.dataTransferencia BETWEEN :dataInicial AND :dataFinal")
+    // Método para buscar transferências por número da conta e período de tempo
     List<Transferencia> findByContaIdAndDataTransferenciaBetween(Long numeroConta, LocalDateTime dataInicial, LocalDateTime dataFinal);
-
 }
